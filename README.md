5894de4a03a943bc78697572ad3a43f566b986e2
14thDovePin <therlaf0409@gmail.com>
2023-11-27 23:48:11 +0800

    Initial Commit
    - Added the following files:
      + .gitignore
      + COMMITLOGS.txt
      + LICENSE
      + Wish-Wosh-Water-System-For-WBM.ino
      + README.md
    - License used is the MIT License.
    - Added contents to `.gitignore`.
    - Added contents to `README.md`.
    
    `Wish-Wosh-Water-System-For-WBM.ino` Notes
    - Included the following libraries and dependencies:
      + SD by Arduino, SparkFun
      + Adafruit Unified Sensor by Adafruit...
      + DHT sensor library by Adafruit
    - Defined the following functions:
      + setup
      + loop
      + sdcm_fail_indicator
      + write_data
      + cycle_indicator
      + poff_ready_indicator
      + check_sdcm
      + pd_th_sensor

---
+++ README.md
@@ -0,0 +1,31 @@
+# Wish Wosh Water System For WBMs (WWWS-WBM)
+The **Wish Wosh Water System For White Button Mushrooms** is an automated sprinkler system designed to log research data and automate watering in mushroom cultivation, specifically *White Button Mushrooms* (WBM).
+
+This system was designed for a group research project of *14thDovePin* back in his final year of high school and its purpose is purely for referencing the source code for the micro controller used, in this case an Arduino Nano.
+## Research Data
+This setup is only designed to log the following data while cultivating WBMs:
+- Date & Time
+- Relative Humidity
+- Temperature
+- Soil Moisture Content
+- Spray Count
+
+Regarding **Date & Time**. Due to the research's time scope, and financial constraints. The code for this system does not support date and time as the hardware didn't include a clock module. The **Date & Time** data will be calculated using the `millis()` function. Therefore, the researchers are required to log the current *Date* and the current *Time* when they power up the micro controller and calculate the final **Date & Time** data for their use case.
+## Indicator Lights
+There are 3 indicator lights accordingly as followed:
+[B] [R<sub>A</sub>] [R<sub>B</sub>]
+
+[B] = Cycle per Second Indicator Light
+[R<sub>A</sub>] = Red Indicator Light A
+[R<sub>B</sub>] = Red Indicator Light B
+
+--- = Off Time
+... = Repeat
+
+**Normal Cycles** - All systems normal.
+[B] --- [B] ...
+**SD Card Error**
+[R<sub>A</sub>] --- [R<sub>B</sub>] --- [R<sub>A</sub>] --- [R<sub>B</sub>] ...
+**Shutdown Ready** - Safe to power off.
+[B] [B] ...
+[R<sub>A</sub>] [R<sub>B</sub>] --- [R<sub>A</sub>] [R<sub>B</sub>] ...
---
+++ Wish-Wosh-Water-System-For-WBM.ino
@@ -0,0 +1,194 @@
+// Dependencies for SD Card Module.
+#include <SPI.h>
+#include <SD.h>
+
+// Dependencies for the DHT Temperature & Humidity Sensor.
+#include <DHT.h>
+
+
+// User defined variables.
+const int base = 2;  // Cycles per Second
+const bool developerMode = true;
+
+
+// Indicator LEDs.
+const int CPS_LED_IP = A3;  // CPS Indicator
+const int A_LED_IP = A4;    // Red Indicator A
+const int B_LED_IP = A5;    // Red Indicator B
+
+// Power off button.
+const int P_OFF = A2;
+
+// SD Card Module setup.
+const int chipSelect = 10;  // D10
+
+// DHT Temperature & Humidity Sensor setup.
+#define DHTPIN 9       // D9
+#define DHTTYPE DHT11  // DHT Sensor Type
+
+DHT dht(DHTPIN, DHTTYPE);
+
+
+// Pre-defined variables.
+const int mult = 1000;
+const int cps = base * mult;
+unsigned long previousMillis = 0;
+
+unsigned long startTime;
+
+
+void setup() {
+  // Setup serial monitor.
+  Serial.begin(9600);
+
+  if (developerMode) {
+    Serial.println("Developer Mode: Active");
+  } else {
+    Serial.println("Developer Mode: Inactive");
+  }
+
+  // Setup indicator pins.
+  pinMode(CPS_LED_IP, OUTPUT);
+  pinMode(A_LED_IP, OUTPUT);
+  pinMode(B_LED_IP, OUTPUT);
+
+  // Setup power off pin.
+  pinMode(P_OFF, INPUT_PULLUP);
+
+  // Initialize SD card module.
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed.");
+    sdcm_fail_indicator();
+  }
+  else {
+    Serial.println("Card initialized.");
+  }
+
+  // Write the necessary file.
+  if (!developerMode) {
+    write_data();
+  }
+
+  // Initialize temperature and humidity sensor.
+  dht.begin();
+}
+
+
+void loop() {
+  // Get the current time
+  unsigned long currentMillis = millis();
+
+  // Check if time exceeded CPS.
+  if (currentMillis - previousMillis >= cps) {
+    // Save the current time.
+    previousMillis = currentMillis;
+
+    // Call functions here.
+    cycle_indicator();
+
+    check_sdcm();
+    pd_th_sensor();
+  }
+
+  // Check if the button is pressed
+  if (digitalRead(P_OFF) == LOW) {
+    poff_ready_indicator();
+    Serial.println("Button pressed!"); // Print to Serial Monitor
+  }
+}
+
+
+void sdcm_fail_indicator() {
+  // SD Card Module Failed Indicator
+  while (true) {
+    digitalWrite(A_LED_IP, HIGH);
+    delay(450);
+    digitalWrite(A_LED_IP, LOW);
+    delay(50);
+
+    digitalWrite(B_LED_IP, HIGH);
+    delay(450);
+    digitalWrite(B_LED_IP, LOW);
+    delay(50);
+  }
+}
+
+
+void write_data() {
+  int fileIndex = 0;
+
+  // Find and create a unique filename.
+  while (SD.exists("data_" + String(fileIndex) + ".txt")) {
+    fileIndex++;
+  }
+  String fileName = "data_" + String(fileIndex) + ".txt";
+
+  // Create file with the unique filename.
+  File dataFile = SD.open(fileName, FILE_WRITE);
+
+  // Write data headers into the file.
+  if (dataFile) {
+    dataFile.print("Time Elapsed,");
+    dataFile.print("Temperature,");
+    dataFile.print("Humidity,");
+    dataFile.print("Soil Moisture,");
+    dataFile.println("Spray Count");
+
+  // Close file and record the start time.
+    dataFile.close();
+    startTime = millis();
+    Serial.println("File Created: `" + fileName + "`.");
+  } else {
+    Serial.println("Error Creating: `" + fileName + "`.");
+  }
+}
+
+
+void cycle_indicator() {
+  // Toggles the CPS LED Indicator.
+  digitalWrite(CPS_LED_IP, HIGH);
+  delay(200);
+  digitalWrite(CPS_LED_IP, LOW);
+}
+
+
+void poff_ready_indicator() {
+  // Allows for safely turning off the device.
+  digitalWrite(CPS_LED_IP, HIGH);
+  while (true) {
+    digitalWrite(A_LED_IP, HIGH);
+    digitalWrite(B_LED_IP, HIGH);
+    delay(300);
+
+    digitalWrite(A_LED_IP, LOW);
+    digitalWrite(B_LED_IP, LOW);
+    delay(700);
+  }
+}
+
+
+void check_sdcm() {
+  // Check SD card module.
+  if (!SD.begin(chipSelect)) {
+    sdcm_fail_indicator();
+  }
+}
+
+
+void pd_th_sensor() {
+  // Return data from DHT11 temperature & humidity sensor.
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+  // Check if readings are valid
+  if (isnan(humidity) || isnan(temperature)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  // Print data to Serial Monitor
+  Serial.print("Relative Humidity: ");
+  Serial.print(humidity);
+  Serial.print("%  Temperature: ");
+  Serial.println(temperature);
+}
